6fcef22d7282214bd27f1b772c0c4fc45e303b1e
PradipKhandare <pradip.khandare@neutrinotechlabs.com>
2024-06-25 16:48:54 +0530

    Added clicking image functionality using camera, showing image on details screen

---
+++ lib/screens/add_place.dart
@@ -0,0 +1,76 @@
+import 'dart:io';
+
+import 'package:favorite_places/providers/user_places.dart';
+import 'package:favorite_places/widgets/image_input.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class AddPlaceScreen extends ConsumerStatefulWidget {
+  const AddPlaceScreen({super.key});
+
+  @override
+  ConsumerState<AddPlaceScreen> createState() => _AddPlaceScreenState();
+}
+
+class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
+  final _titleController = TextEditingController();
+  File? _selectedImage;
+
+  @override
+  void dispose() {
+    super.dispose();
+    _titleController.dispose();
+  }
+
+  void _savePlace() {
+    final enteredText = _titleController.text;
+
+    if (enteredText.isEmpty || _selectedImage == null) {
+      return;
+    }
+
+    ref.read(userPlacesProvider.notifier).addPlace(enteredText, _selectedImage!);
+
+    Navigator.of(context).pop();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add new place'),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          children: [
+            TextField(
+              decoration: const InputDecoration(
+                labelText: 'Title',
+              ),
+              controller: _titleController,
+              style:
+                  TextStyle(color: Theme.of(context).colorScheme.onBackground),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            ImageInput(
+              onPickImage: (image) {
+                _selectedImage = image;
+              },
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            ElevatedButton.icon(
+              onPressed: _savePlace,
+              label: const Text('Add place'),
+              icon: const Icon(Icons.add),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/places_detail.dart
@@ -0,0 +1,27 @@
+import 'package:favorite_places/models/place.dart';
+import 'package:flutter/material.dart';
+
+class PlacesDetailScreen extends StatelessWidget {
+  const PlacesDetailScreen({super.key, required this.place});
+
+  final Place place;
+
+  @override
+  Widget build(BuildContext context) {
+   return Scaffold(
+     appBar: AppBar(
+       title: Text(place.title),
+     ),
+     body: Stack(
+       children: [
+         Image.file(
+           place.image,
+           fit: BoxFit.cover,
+           height: double.infinity,
+           width: double.infinity,
+         ),
+       ],
+     ),
+   );
+  }
+}
